005ea918c688fcd481c2d57a150e5554304551bd
Tuyen <michael.ross.mckay@gmail.com>
2020-12-19 23:39:46 -0500

    add theme persistence and import cookie funcs

--- public/js/themes.js
+++ public/js/themes.js
@@ -1,16 +1,26 @@
-let theme_tag = document.getElementById('theme')
+import {get_cookie, set_cookie} from './cookies.js';
+
 let themes = [
   '/css/dark.css',
   '/css/light.css',
-]
-let theme_index = 0
+];
+let theme_tag = document.getElementById('theme');
+let theme_index = 0;
 
 
-function main() {
-} main();
+function setup() {
+  theme_index = get_cookie("theme") == "" ? 0 : parseInt(get_cookie("theme"))
+  set_theme(theme_index)
+} setup();
+
+
+function set_theme(index) {
+  theme_tag.setAttribute('href', themes[theme_index]);
+  set_cookie("theme", theme_index, 365)
+}
 
 
 function swap_theme() {
   theme_index = theme_index + 1 == themes.length ? 0 : theme_index + 1
-  theme_tag.setAttribute('href', themes[theme_index]);
-}
+  set_theme(theme_index)
+} window.swap_theme = swap_theme;
